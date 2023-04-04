402f669ba20b8f9e996014a69ea1bb1340556ab2
JGM <kotori438@naver.com>
2023-04-04 12:32:40 +0900

    0404 2교시

--- 0404.py
+++ 0404.py
@@ -44,8 +44,113 @@ print(int(value))
 print(bool(0),bool(1),bool(1.33),bool(-12))
 print(bool("dasgsagsd"),bool("-1"),bool(""))#""은 False 이다
 '''
+'''
+if 조건:
+    실행문
+else 
+    실행문2   
+'''
+'''
+#오전? 오후?
+hour = 13
+if hour < 12:
+    print("12시가 않지나서 오전입니다")
+elif hour<19 : 
+    print("12시가 지나고 ,18시가 안지났으니 오후입니다.")
 
+else :
+    print("18시가 지났으니 저녁입니다.")
 
+#장학금
+#score < 200 50만원을 줌
+#200 <= score < 400 100만원
+#400 <= score < 500 10000만원줌
+score_str = input("점수는? ")
+print(type(score_str))
+score = int(score_str)
+if score < 200 :
+    print("50만원획득")
+elif score < 400 :
+    print("100만원 획득")
+else :
+    print("1000만원 획득")
+'''
+'''
+#점심식사
+answer = input("서브웨이 먹을래?")
+if answer == 'yes'or'응':
+    print("8호관 1층")
+else :
+    again_anser = input("학식 먹을래?")
+    if again_anser == 'yes'or'응':
+        print("8호관 3층")
+    else :
+        print("넌 굶어라")
+'''
 
+'''
+#반복문
+#for
+for i in 1,3,4,5,6,8 :
+    print(i)
+        
+print("range1") 
+for i in range(0,11,1) :
+    print(i)
+sum=0
+print("range2")    
+for i in range(11) :
+    sum = sum + i
+    print(i,"번쨰 sum은",sum)
+else :
+    print("for 문의 조건이 만족하지않습니다.")
+    print("i가 range(11)에서 벗어남.")
+    print("for문이 break나 countinue로 종료된게 아니라 정상종료시에만 실행")    
+print("sum: ", sum)
+'''
+'''
+#놀이기구 탑승
+#4명 탑승가능
+#키 150이상
+i=0
+while i<=3 :
+    print("4명이 탈수있는 놀이기구에 어서오세요")
+    print("현재",i,"명 탑승중입니다")
+    print("남은좌석수 : ",4-i)
+    a_str=input("당신의 키는?")
+    a=int(a_str)
+    if a>150 :
+        i=i+1
+        print("놀이기구에 탑승했습니다")
+    else :
+        print("키가 기준에 맞지않아 탈승 할 수 없습니다.")
+else :
+    print("탑승인원이 4명이 꽉차서 출발합니다.")
+    print("즐거운 시간되세요")
+'''
 
+'''
+while 조건:
+    수행문
+'''
+'''
+i=10
+while i>5:
+    print(i,"는 5보다 크다")
+    i = i-1
+n=1
+while n<=5:
+    print(n, end='  **   ')
+    n=n+1
+else:
+    print("while이 잘 끝났습니다")
+    print("현재 n의 값은 ",n,"입니다")
+'''
 
+#continue , break
+#반복문 중간에 반복을 더이상 하지않고 실행을 종료가 목적
+#반복만 안쪽에서 실행된다.
+#주로 if문 안쪽에서 사용됨 if안에 있지만 if를 끝내는게아니라 그위에 for while 을 끝내는거임
+#for while 둘다 사용
+#continue는 n차시중 만나면 n+1 차시를 하는거
+#break는 n차시중에 그냥 나가는거 
