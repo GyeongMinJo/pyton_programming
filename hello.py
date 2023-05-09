a69b69044502a4aad79999596c9943c3d94f0d63
JGM <kotori438@naver.com>
2023-05-09 12:36:23 +0900

    0509수업내용

---
+++ hello.py
@@ -0,0 +1,14 @@
+#hello
+
+def helloworld():
+    print("hello world!!")
+    
+
+
+if __name__ == "__main__":
+    print(__name__)
+    print("오늘은 module 처음 배운 날입니다.")
+    print("오늘은 날씨가 24도 입니다.")
+    helloworld()
+else:
+    print("hello.py __name__",__name__)
